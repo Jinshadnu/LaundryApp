0ae6a114fa378fe127e9bd1d7ce7d1d4abea2fe0
Jinshadnu <jinshad.channanath@gmail.com>
2020-11-09 16:56:42 +0530

    change password ui changed

--- app/src/main/java/com/example/laundryapp/user/AboutUsActivity.java
+++ app/src/main/java/com/example/laundryapp/user/AboutUsActivity.java
@@ -1,17 +1,33 @@
 package com.example.laundryapp.user;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.laundryapp.R;
+import com.example.laundryapp.databinding.ActivityAboutUsBinding;
 
 public class AboutUsActivity extends AppCompatActivity {
-
+public ActivityAboutUsBinding aboutUsBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+        //setContentView(R.layout.activity_about_us);
+
+        aboutUsBinding= DataBindingUtil.setContentView(this,R.layout.activity_about_us);
+
+        aboutUsBinding.layoutBase.textTitle.setText("Contact us");
+
+        aboutUsBinding.layoutBase.toolbar.setNavigationOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        aboutUsBinding.layoutBase.toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+
+
+
+
 
 
 
