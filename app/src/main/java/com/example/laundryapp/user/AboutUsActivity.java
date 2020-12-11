043c762872ddcc6e7d647f0c083bae44412f9752
Jinshadnu <jinshad.channanath@gmail.com>
2020-12-11 15:43:16 +0530

    ABout us page ui changed

--- app/src/main/java/com/example/laundryapp/user/AboutUsActivity.java
+++ app/src/main/java/com/example/laundryapp/user/AboutUsActivity.java
@@ -9,9 +9,16 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.laundryapp.R;
 import com.example.laundryapp.databinding.ActivityAboutUsBinding;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class AboutUsActivity extends AppCompatActivity {
 public ActivityAboutUsBinding aboutUsBinding;
@@ -29,6 +36,13 @@ public ActivityAboutUsBinding aboutUsBinding;
         });
 
         aboutUsBinding.layoutBase.toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+
+        aboutUsBinding.textOpenMap.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/fSi985xb7m63GmCw5"));
+            startActivity(browserIntent);
+        });
+
+
 
         aboutUsBinding.imageViewPhone.setOnClickListener(view -> {
             Intent callIntent = new Intent(Intent.ACTION_CALL);
